68e79ea73e61bc26cf8b1f8131e824ff345e13a6
0xturboblitz <florent.tavernier@gmail.com>
2025-09-27 14:57:25 -0700

    new animation

--- src/components/P5Runner.tsx
+++ src/components/P5Runner.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef } from 'react';
 import type p5 from 'p5';
 import { P5SketchSpec } from '@/lib/animation';
 
